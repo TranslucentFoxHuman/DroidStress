773d698d0794e7aeff9378327e5856c46be58d63
TlFoxHuman <tlfoxhuman@noreply.localhost>
2026-03-23 04:52:09 +0900

    First commit

---
+++ app/src/main/java/net/tlfoxhuman/droidstress/MainActivity.kt
@@ -0,0 +1,141 @@
+/*
+ * DroidStress
+ * Copyright (C) 2026 半狐 (半透明狐人間,TlFoxHuman,TranslucentFoxHuman)
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.tlfoxhuman.droidstress
+
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.res.Configuration
+import android.os.Build
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.core.widget.addTextChangedListener
+
+class MainActivity : AppCompatActivity() {
+
+    // Global variables
+    private var isTextChanged: Boolean = false
+
+    private fun reloadServiceStatus() {
+        //// Read current service status
+        if (StressService.isRunning) {
+            findViewById<Button>(R.id.startbutton).setText(R.string.stop)
+        } else {
+            findViewById<Button>(R.id.startbutton).setText(R.string.start)
+        }
+        //// Restore running threads
+        findViewById<EditText>(R.id.threadsInput).setText(StressService.threadCount.toString())
+    }
+
+    // Notification related codes
+    // Ahhhhh!! F**k Google!!! I wasted more than an hour writing code that controls the permissions of notifications and notifications itself!! Android is evil because it has a lot of destructive changes. At the same time, all object-oriented programming languages are evil. Yes, They are EVIL. I LOVE C. I HATE Kotlin.
+    private val permissionReq = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),) {}
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.notif_channel_name)
+            val descriptionText = getString(R.string.notif_channel_description)
+            val importance = NotificationManager.IMPORTANCE_NONE
+            val channel = NotificationChannel("net.tlfoxhuman.droidstress.notif.fgservice", name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    // int main(void) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // init
+        reloadServiceStatus()
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            permissionReq.launch(Manifest.permission.POST_NOTIFICATIONS)
+        }
+        createNotificationChannel()
+        ////F**k Google!! Don't make unnecessary changes!!
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+            fun isLight(): Boolean {
+                val uiMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+                return uiMode == Configuration.UI_MODE_NIGHT_YES
+            }
+            val wic = WindowInsetsControllerCompat(window, window.decorView)
+            wic.isAppearanceLightStatusBars = !isLight()
+        }
+
+
+        val svcobj = Intent(application, StressService::class.java)
+
+        findViewById<Button>(R.id.startbutton).setOnClickListener {
+            if (StressService.isRunning) {
+                // Stop service
+                stopService(svcobj)
+                if (isTextChanged) {
+                    StressService.threadCount = findViewById<EditText>(R.id.threadsInput).text.toString().toIntOrNull() ?: 8
+                    reloadServiceStatus()
+                    startService(svcobj)
+                    findViewById<Button>(R.id.startbutton).setText(R.string.stop)
+                    isTextChanged = false
+                } else {
+                    findViewById<Button>(R.id.startbutton).setText(R.string.start)
+                }
+            } else {
+                // Start service
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                    permissionReq.launch(Manifest.permission.POST_NOTIFICATIONS)
+                    return@setOnClickListener
+                }
+                StressService.threadCount = findViewById<EditText>(R.id.threadsInput).text.toString().toIntOrNull() ?: 8
+                reloadServiceStatus()
+                startService(svcobj)
+                findViewById<Button>(R.id.startbutton).setText(R.string.stop)
+            }
+        }
+
+        findViewById<EditText>(R.id.threadsInput).addTextChangedListener {
+            if (StressService.isRunning) {
+                isTextChanged = true
+                findViewById<Button>(R.id.startbutton).setText(R.string.restart)
+            }
+        }
+
+    }
+
+
+
+}
---
+++ app/src/main/java/net/tlfoxhuman/droidstress/StressService.kt
@@ -0,0 +1,85 @@
+/*
+ * DroidStress
+ * Copyright (C) 2026 半狐 (半透明狐人間,TlFoxHuman,TranslucentFoxHuman)
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.tlfoxhuman.droidstress
+
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+class StressService : Service() {
+    companion object {
+        @Volatile var isRunning = false
+        var threadCount : Int = 8
+    }
+
+    suspend fun stress() {
+        var number: Long = System.currentTimeMillis()
+        while (StressService.isRunning) {
+            if (number % 4 == 0L) {
+                number = number*2
+            } else if (number % 3 == 0L) {
+                number = number/2
+            } else if (number % 2 == 0L) {
+                number = number + 1
+            } else {
+                number = number -1
+            }
+        }
+    }
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val backToMainIntent = Intent(this, MainActivity::class.java).let {
+            PendingIntent.getActivity(this, 0, it, PendingIntent.FLAG_IMMUTABLE)
+        }
+        var notifBuilder = NotificationCompat.Builder(this, "net.tlfoxhuman.droidstress.notif.fgservice")
+            .setSmallIcon(R.drawable.ic_launcher_monochrome)
+            .setContentTitle(getString(R.string.notif_fgsvc_title))
+            .setContentText(getString(R.string.notif_fgsvc_desc))
+            .setContentIntent(backToMainIntent)
+            .setPriority(NotificationCompat.PRIORITY_MIN).build()
+
+        startForeground(1,notifBuilder)
+
+        StressService.isRunning = true
+        val scope = CoroutineScope(Dispatchers.Default)
+        val jobs = (1..threadCount).map {
+            scope.launch { stress() }
+        }
+        //runBlocking { jobs.joinAll() }
+        return START_NOT_STICKY
+    }
+
+    override fun onDestroy() {
+        StressService.isRunning = false
+        super.onDestroy()
+    }
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
