63c514173d7d7674a35e6a9cde1c51d2a44d8af9
TlFoxHuman <tlfoxhuman@noreply.localhost>
2026-06-25 20:55:11 +0900

    1.6.3 : Changed to scroll main screen when display is narrow.

--- app/src/main/java/net/tlfoxhuman/droidstress/MainActivity.kt
+++ app/src/main/java/net/tlfoxhuman/droidstress/MainActivity.kt
@@ -35,6 +35,8 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.core.widget.addTextChangedListener
 import kotlin.concurrent.thread
@@ -143,6 +145,11 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         // init
         svcobj = Intent(application, StressService::class.java)
